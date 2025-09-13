50c7d722e0d05ebab9f087f3b52ae1eab763e409
Bohdan-Kozlo <kozlo.bogdann@gmail.com>
2025-09-13 16:54:52 +0300

    add configuration management and database connection setup and base struct

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"go-deadlink-scanner/internal/config"
+	"log"
 
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+
+	connect, err := sql.Open("postgres", cfg.DBUrl)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	defer connect.Close()
+
+	// queries := db.New(connect)
+
+	app := fiber.New(fiber.Config{
+		AppName: "Go Dead Link Scanner",
+	})
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	log.Printf("Server started on :%s", cfg.Port)
+	if err := app.Listen(":" + cfg.Port); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBUrl         string
+	Port          string
+	SessionSecret string
+	SessionMaxAge time.Duration
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	dbUrl := GetEnv("DB_URL", "postgres://postgres:postgres@localhost:5432/linkchecker?sslmode=disable")
+	sessionSecret := GetEnv("SESSION_SECRET", "supersecretkey")
+	serverPort := GetEnv("SERVER_PORT", "8080")
+
+	return &Config{
+		DBUrl:         dbUrl,
+		Port:          serverPort,
+		SessionSecret: sessionSecret,
+		SessionMaxAge: 7 * 24 * time.Hour,
+	}
+}
+
+func GetEnv(key, defaultValue string) string {
+	if value, exist := os.LookupEnv(key); exist {
+		return value
+	}
+	log.Printf("⚠️ %s not set, using default: %s", key, defaultValue)
+	return defaultValue
+}
